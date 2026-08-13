2170e672ddb1679624158b3943e29d31b9a41bb2
paul sunny <it@mannarubber.com>
2026-08-13 08:54:35 +0530

    stor the data

--- server/config/db.js
+++ server/config/db.js
@@ -1,19 +1,20 @@
 import mongoose from "mongoose";
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 
 dotenv.config();
 
+const DEFAULT_LOCAL_URI = "mongodb://127.0.0.1:27017/stockmaster";
+
 const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log("MongoDB Connected")
-        
-    } catch (error) {
-         console.error("MongoDB Connection Error:", error.message);
+  const mongoUri = process.env.MONGO_URI || DEFAULT_LOCAL_URI;
+
+  try {
+    await mongoose.connect(mongoUri);
+    console.log("MongoDB Connected");
+  } catch (error) {
+    console.error("MongoDB Connection Error:", error.message);
     process.exit(1);
-        
-    }
-    
-}
+  }
+};
 
 export default connectDB;
